60e3c9cfac9422b5d022fa558068ad20ffcc1207
Bruno Bartolomasi <brunobrt@hotmail.com>
2020-06-13 20:53:56 -0300

    Add blog/about.htlm and blog/home.html

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Bruno Bartolomasi',
+        'title': 'First Blog Post',
+        'content': 'This is my fist blog post ever!',
+        'date_posted': 'June 13th, 2020'
+    },
+    {
+        'author': 'Bruno R Bartolomasi',
+        'title': 'Second Blog Post',
+        'content': 'This is my second blog post ever!',
+        'date_posted': 'June 14th, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
